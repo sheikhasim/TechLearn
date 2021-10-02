1f27d6e19e34c32649725b71f1fa3d114516b18c
MOHAMMAD ASIM SHEIKH <sheikhasim411@gmail.com>
2021-10-02 16:22:26 +0530

    3rd commit

--- c4app/admin.py
+++ c4app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from c4app.models import UserProfileInfo
+from django.contrib import admin
+from django.contrib.auth.forms import UserCreationForm,UserChangeForm
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
+from .models import User
 
 
 # Register your models here.
